e636d34db864b9d3f4436411ce148c524881dbc1
2heeng <yiheeng0104@gmail.com>
2024-05-01 10:24:24 +0900

    Initial commit

---
+++ app/src/main/java/com/cookandroid/myappactivity1/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.cookandroid.myappactivity1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        var btnNewActivity = findViewById<Button>(R.id.btnNewActivity)
+        btnNewActivity.setOnClickListener {
+            var intent = Intent(applicationContext, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
